a742fff2fd72a0e50503576c65de66571a316ff0
silvamaciel <macieljunio.si@gmail.com>
2023-10-21 14:02:15 -0300

    Projeto App Produtos Java

---
+++ src/test/java/br/com/ehmf/appProdutos/AppProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ehmf.appProdutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
